f61bc88a38a4ef71783d68e9b4aa6034c298eb6d
Vincent Lai <vincentlai91@hotmail.com>
2023-04-21 17:08:19 +0800

    added buttons for Home Draw and Away selections

--- oasisbet-frontend/src/app/odds/odds-landing/odds-landing.component.ts
+++ oasisbet-frontend/src/app/odds/odds-landing/odds-landing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { OddsSideNavComponent } from '../odds-side-nav/odds-side-nav.component';
 import { SharedVarService } from 'src/app/services/shared-var.service';
 import { BetEvent } from 'src/app/model/bet-event.model';
+import { H2HEventOdds } from 'src/app/model/h2h-event-odds.model';
 
 @Component({
   selector: 'app-odds-landing',
@@ -12,12 +13,54 @@ export class OddsLandingComponent implements OnInit {
 
   competitionType: string;
   public events : BetEvent[];
+  public betEvent: BetEvent;
+  public betEvent2: BetEvent;
+  public betEvent3: BetEvent;
 
   constructor(public sharedVar: SharedVarService) { 
     this.competitionType = this.sharedVar.COMP_HEADER_EPL;
   }
 
   ngOnInit(): void {
+    this.betEvent = new BetEvent;
+    this.betEvent2 = new BetEvent;
+    this.betEvent3 = new BetEvent;
+    
+    this.betEvent.competition = 'English Premier League';
+    this.betEvent.eventDesc = 'Chelsea vs Manchester Utd';
+    this.betEvent.startTime = new Date(2023, 6, 4, 22, 0, 0, 0);
+    this.betEvent.eventId = 1002;
+
+    this.betEvent2.competition = 'English Premier League';
+    this.betEvent2.eventDesc = 'Arsenal vs Tottenham Hotspur';
+    this.betEvent2.startTime = new Date(2023, 6, 4, 22, 0, 0, 0);
+    this.betEvent2.eventId = 1003;
+
+    this.betEvent3.competition = 'English Premier League';
+    this.betEvent3.eventDesc = 'Everton vs Manchester City';
+    this.betEvent3.startTime = new Date(2023, 6, 4, 22, 0, 0, 0);
+    this.betEvent3.eventId = 1004;
+
+    this.betEvent.h2hEventOdds = new H2HEventOdds;
+    this.betEvent.h2hEventOdds.homeOdds = 2.20;
+    this.betEvent.h2hEventOdds.drawOdds = 3.25;
+    this.betEvent.h2hEventOdds.awayOdds = 2.75;
+
+    this.betEvent2.h2hEventOdds = new H2HEventOdds;
+    this.betEvent2.h2hEventOdds.homeOdds = 1.88;
+    this.betEvent2.h2hEventOdds.drawOdds = 3.25;
+    this.betEvent2.h2hEventOdds.awayOdds = 3.05;
+
+    this.betEvent3.h2hEventOdds = new H2HEventOdds;
+    this.betEvent3.h2hEventOdds.homeOdds = 4.15;
+    this.betEvent3.h2hEventOdds.drawOdds = 3.30;
+    this.betEvent3.h2hEventOdds.awayOdds = 1.65;
+
+    this.events = [];
+    this.events.push(this.betEvent);
+    this.events.push(this.betEvent2);
+    this.events.push(this.betEvent3);
+
   }
 
   readCompType(compType: string){ 
