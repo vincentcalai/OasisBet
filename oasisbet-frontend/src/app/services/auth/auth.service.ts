c802e9155ad3b50c3c3342543fdeea1a18eef695
Vincent Lai <vincentlai91@hotmail.com>
2023-05-06 23:20:20 +0800

    added authentication logics added to display logged in user on main menu header

--- oasisbet-frontend/src/app/services/auth/auth.service.ts
+++ oasisbet-frontend/src/app/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
+import { Router } from '@angular/router';
 
 export const AUTH_USER = 'authenticateUser';
 export const TOKEN = 'token';
@@ -10,10 +11,35 @@ export const TOKEN = 'token';
 
 export class AuthService {
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private router: Router) { }
 
     jwtAuthenticate(username: string, password: string) {
       return this.apiService.jwtAuthenticate(username, password);
+    }
+
+    isUserLoggedIn(){
+      let user =  sessionStorage.getItem(AUTH_USER);
+      return !(user === null);
+    }
+  
+    getAuthenticationUser(){
+      return sessionStorage.getItem(AUTH_USER);
+    }
+  
+    getAuthenticationToken(){
+      if(this.getAuthenticationUser()){
+        return sessionStorage.getItem(TOKEN);
+      }
+      return null;
+    }
+
+    logout(){
+      if(confirm("Are you sure to logout?")) {
+        console.log("logout ok");
+        sessionStorage.removeItem(AUTH_USER);
+        sessionStorage.removeItem(TOKEN);
+        this.router.navigate(['/']);
+      }
     }
 
 }
--- oasisbet-frontend/src/app/views/c-main-menu/main-menu.component.ts
+++ oasisbet-frontend/src/app/views/c-main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-main-menu',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.authService.logout();
+  }
 }
