af8f78cb751b758acc6349d056e7f66a89253418
Vincent Lai <vincentlai91@hotmail.com>
2023-06-03 17:45:16 +0800

    sort array based on commence time of events. then apply sequence to the event id in Mongo DB implemented mongodb config setting in application.properties, and extract from config classes

---
+++ betting/src/main/java/com/oasisbet/betting/util/MongoDBConfig.java
@@ -0,0 +1,39 @@
+package com.oasisbet.betting.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MongoDBConfig {
+	@Value("${mongodb.client.connectionUrl}")
+	private String connectionUrl;
+	@Value("${mongodb.client.databaseName}")
+	private String databaseName;
+	@Value("${mongodb.client.collectionName}")
+	private String collectionName;
+
+	public String getConnectionUrl() {
+		return connectionUrl;
+	}
+
+	public void setConnectionUrl(String connectionUrl) {
+		this.connectionUrl = connectionUrl;
+	}
+
+	public String getDatabaseName() {
+		return databaseName;
+	}
+
+	public void setDatabaseName(String databaseName) {
+		this.databaseName = databaseName;
+	}
+
+	public String getCollectionName() {
+		return collectionName;
+	}
+
+	public void setCollectionName(String collectionName) {
+		this.collectionName = collectionName;
+	}
+
+}
--- betting/src/main/java/com/oasisbet/betting/util/MongoDBConnection.java
+++ betting/src/main/java/com/oasisbet/betting/util/MongoDBConnection.java
@@ -1,31 +1,46 @@
 package com.oasisbet.betting.util;
 
+import javax.annotation.PostConstruct;
+
 import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+@Component
 public class MongoDBConnection {
+
 	private static MongoDBConnection instance = null;
 	private MongoClient mongoClient;
 	private MongoDatabase database;
 	private MongoCollection<Document> collection;
 
-	private MongoDBConnection() {
+	@Autowired
+	private MongoDBConfig mongoDBConfig;
+
+	@PostConstruct
+	public void init() {
+		MongoDBConnection.instance = new MongoDBConnection(mongoDBConfig);
+	}
+
+	@Autowired
+	private MongoDBConnection(MongoDBConfig mongoDBConfig) {
 		// private constructor to prevent instantiation outside of the class
-		this.mongoClient = MongoClients.create("mongodb://localhost:27017");
-		this.database = mongoClient.getDatabase("OasisBet");
-		this.collection = database.getCollection("sports_event_mapping");
+		this.mongoClient = MongoClients.create(mongoDBConfig.getConnectionUrl());
+		this.database = mongoClient.getDatabase(mongoDBConfig.getDatabaseName());
+		this.collection = database.getCollection(mongoDBConfig.getCollectionName());
 	}
 
 	public static synchronized MongoDBConnection getInstance() {
 		// lazy initialization - only create instance when it is requested for the first
 		// time
-		if (instance == null) {
-			instance = new MongoDBConnection();
-		}
+//		if (instance == null) {
+//			instance = new MongoDBConnection(mongoDBConfig);
+//		}
 		return instance;
 	}
 
