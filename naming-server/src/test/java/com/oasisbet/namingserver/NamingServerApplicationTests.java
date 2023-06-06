826c10cb17be253077da4d78e11fc44718854c28
Vincent Lai <vincentlai91@hotmail.com>
2023-06-06 09:01:20 +0800

    added naming server as new project

---
+++ naming-server/src/test/java/com/oasisbet/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasisbet.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
