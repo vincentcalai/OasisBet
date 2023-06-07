63ff26dcd07a5ef88ea84fb28826d8d141f40cae
Vincent Lai <vincentlai91@hotmail.com>
2023-06-07 10:17:35 +0800

    added new unit test

---
+++ account/src/test/java/com/oasisbet/account/controller/TestAccountController.java
@@ -0,0 +1,66 @@
+package com.oasisbet.account.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oasisbet.account.TestWithSpringBoot;
+import com.oasisbet.account.model.AccountVO;
+import com.oasisbet.account.model.response.AccountRestResponse;
+import com.oasisbet.account.service.AccountService;
+import com.oasisbet.account.util.Constants;
+
+public class TestAccountController extends TestWithSpringBoot {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private AccountService accountService;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	public void testRetrieveAccDetailsSuccess() throws Exception {
+		String username = "TESTUSER";
+
+		AccountRestResponse expectedResponse = new AccountRestResponse();
+		AccountVO accountVo = new AccountVO();
+		accountVo.setUsrId(3L);
+		expectedResponse.setAccount(accountVo);
+
+		Mockito.when(accountService.retrieveUserAccountByUsername(Mockito.any(String.class))).thenReturn(accountVo);
+
+		MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/account/retrieveAccDetails")
+				.contentType(MediaType.APPLICATION_JSON).param("user", username);
+
+		String response = objectMapper.writeValueAsString(expectedResponse);
+		mockMvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().json(response));
+	}
+
+	@Test
+	public void testRetrieveAccDetailsFail() throws Exception {
+		String username = "TESTUSER";
+
+		AccountRestResponse expectedResponse = new AccountRestResponse();
+		expectedResponse.setStatusCode(1);
+		expectedResponse.setResultMessage(Constants.ERR_USER_ACC_NOT_FOUND);
+
+		Mockito.when(accountService.retrieveUserAccountByUsername(Mockito.any(String.class))).thenReturn(null);
+
+		MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/account/retrieveAccDetails")
+				.contentType(MediaType.APPLICATION_JSON).param("user", username);
+
+		String response = objectMapper.writeValueAsString(expectedResponse);
+		mockMvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().json(response));
+	}
+}
