8b48e917a364bef4933c2ad8834fdc64cb4f94b1
Vincent Lai <vincentlai91@hotmail.com>
2023-06-08 13:41:05 +0800

    added test cases

--- account/src/test/java/com/oasisbet/account/service/TestAccountService.java
+++ account/src/test/java/com/oasisbet/account/service/TestAccountService.java
@@ -1,17 +1,30 @@
 package com.oasisbet.account.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.oasisbet.account.TestWithSpringBoot;
+import com.oasisbet.account.dao.IAccountOtherTrxDao;
 import com.oasisbet.account.model.AccountVO;
 
 public class TestAccountService extends TestWithSpringBoot {
 
 	@Autowired
 	AccountService accountService;
+
+	@InjectMocks
+	AccountService mockAccountService;
+
+	@Mock
+	private IAccountOtherTrxDao accountOtherTrxDao;
 
 	@Test
 	void testRetrieveUserAccountByUsernameSuccess() {
@@ -32,6 +45,34 @@ public class TestAccountService extends TestWithSpringBoot {
 		String user = "TESTUSERA";
 		AccountVO accountVo = accountService.retrieveUserAccountByUsername(user);
 		assertEquals(null, accountVo);
+	}
+
+	@Test
+	void testRetrieveYtdAmountsSuccess() {
+		Long accId = 100002L;
+		Double ytdDepositAmount = 100.0;
+		Double ytdWithdrawalAmount = 50.0;
+		when(accountOtherTrxDao.findYtdDeposit(Mockito.any(Long.class), Mockito.any(Date.class)))
+				.thenReturn(ytdDepositAmount);
+		when(accountOtherTrxDao.findYtdWithdrawal(Mockito.any(Long.class), Mockito.any(Date.class)))
+				.thenReturn(ytdWithdrawalAmount);
+
+		AccountVO accountVo = mockAccountService.retrieveYtdAmounts(accId);
+
+		assertEquals(ytdDepositAmount, accountVo.getYtdDepositAmt());
+		assertEquals(ytdWithdrawalAmount, accountVo.getYtdWithdrawalAmt());
+	}
+
+	@Test
+	void testRetrieveYtdAmountsFail() {
+		Long accId = 100002L;
+		when(accountOtherTrxDao.findYtdDeposit(Mockito.any(Long.class), Mockito.any(Date.class))).thenReturn(null);
+		when(accountOtherTrxDao.findYtdWithdrawal(Mockito.any(Long.class), Mockito.any(Date.class))).thenReturn(null);
+
+		AccountVO accountVo = mockAccountService.retrieveYtdAmounts(accId);
+
+		assertEquals(0.0, accountVo.getYtdDepositAmt());
+		assertEquals(0.0, accountVo.getYtdWithdrawalAmt());
 	}
 
 }
