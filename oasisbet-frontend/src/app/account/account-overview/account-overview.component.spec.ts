60a2f8c4312a4917e341b411429c3df81c78aa4f
Vincent Lai <vincentlai91@hotmail.com>
2023-06-11 17:48:15 +0800

    resolved angular unit test errors

--- oasisbet-frontend/src/app/account/account-overview/account-overview.component.spec.ts
+++ oasisbet-frontend/src/app/account/account-overview/account-overview.component.spec.ts
@@ -4,6 +4,8 @@ import { AccountOverviewComponent } from './account-overview.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ACC_DETAILS } from 'src/app/services/auth/auth.service';
+import { AccountModel } from 'src/app/model/account.model';
 
 describe('AccountOverviewComponent', () => {
   let component: AccountOverviewComponent;
@@ -27,6 +29,9 @@ describe('AccountOverviewComponent', () => {
   });
 
   it('should create', () => {
+    let accountModel = new AccountModel();
+    accountModel.accId = 1;
+    sessionStorage.setItem(ACC_DETAILS, JSON.stringify(accountModel));
     expect(component).toBeTruthy();
   });
 });
--- oasisbet-frontend/src/app/odds/odds-bet-slip/odds-bet-slip.component.spec.ts
+++ oasisbet-frontend/src/app/odds/odds-bet-slip/odds-bet-slip.component.spec.ts
@@ -4,6 +4,7 @@ import { OddsBetSlipComponent } from './odds-bet-slip.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 describe('OddsBetSlipComponent', () => {
   let component: OddsBetSlipComponent;
@@ -24,10 +25,12 @@ describe('OddsBetSlipComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OddsBetSlipComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    const mockInitBetSlip = of<void>();
+    component.initBetSlip = mockInitBetSlip;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
