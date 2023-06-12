88363d455896237fbb75f73def79b6312fb2570f
Vincent Lai <vincentlai91@hotmail.com>
2023-06-12 13:22:09 +0800

    fixed unit test accId is null issue

--- oasisbet-frontend/src/app/account/trx-hist/trx-hist.component.spec.ts
+++ oasisbet-frontend/src/app/account/trx-hist/trx-hist.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TrxHistComponent } from './trx-hist.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { ACC_DETAILS } from 'src/app/services/auth/auth.service';
+import { AccountModel } from 'src/app/model/account.model';
 
 describe('TrxHistComponent', () => {
   let component: TrxHistComponent;
@@ -26,6 +29,9 @@ describe('TrxHistComponent', () => {
   });
 
   it('should create', () => {
+    let account = new AccountModel;
+    account.accId = 1;
+    sessionStorage.setItem(ACC_DETAILS, JSON.stringify(account));
     expect(component).toBeTruthy();
   });
 });
