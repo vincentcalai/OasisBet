538812bbcdd98774b187a53df4ba826065916a17
Vincent Lai <vincentlai91@hotmail.com>
2023-06-14 14:24:11 +0800

    added unit tests

--- oasisbet-frontend/src/app/account/account-login/account-login.component.spec.ts
+++ oasisbet-frontend/src/app/account/account-login/account-login.component.spec.ts
@@ -4,7 +4,10 @@ import { AccountLoginComponent } from './account-login.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedVarService } from 'src/app/services/shared-var.service';
-import { BehaviorSubject, of } from 'rxjs';
+import { BehaviorSubject, of, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ACC_DETAILS, AUTH_USER, TOKEN } from 'src/app/services/auth/auth.service';
+import { AccountModel } from 'src/app/model/account.model';
 
 describe('AccountLoginComponent', () => {
   let component: AccountLoginComponent;
@@ -30,6 +33,13 @@ describe('AccountLoginComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    sessionStorage.removeItem(AUTH_USER);
+    sessionStorage.removeItem(TOKEN);
+    sessionStorage.removeItem(ACC_DETAILS);
+    component.ngOnDestroy();
+  })
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -49,4 +59,70 @@ describe('AccountLoginComponent', () => {
     fixture.detectChanges();
     expect(component.responseMsg).toBe('Login Success');
   });
+
+  it('when login is successful, should retrieve account details', () => {
+    const token = {
+      "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJDSE9PTkFOTiIsImV4cCI6MTY3MTA3OTg4NSwiaWF0IjoxNjcwNDc5ODg1fQ.zl_AJFETUvw1WxMjPSgmSb9tTLUjFwg6AHNwS358DQL9kLWs-zYrjG4aPXIWgRlpWM4W0rCx0S0HlFkIJBWfoQ"
+    };
+    spyOn(component, 'retrieveAccDetails');
+    spyOn(component.authService, 'jwtAuthenticate').and.returnValue(of(token));
+    component.handleJWTAuthLogin();
+    expect(component.retrieveAccDetails).toHaveBeenCalledTimes(1);
+    expect(component.errorMsg).toBeNull();
+  });
+
+  it('when login is unsuccessful, should not retrieve account details, and should throw error', () => {
+    const error = new HttpErrorResponse({ status: 500 });
+    spyOn(component, 'retrieveAccDetails');
+    spyOn(component.authService, 'jwtAuthenticate').and.returnValue(throwError(error));
+    component.handleJWTAuthLogin();
+    expect(component.retrieveAccDetails).toHaveBeenCalledTimes(0);
+    expect(component.errorMsg).toBe("Please enter a valid credential. Login failed.");
+  });
+
+  it('when user click sign up user, navigate to sign up user screen', () => {
+    spyOn(component.router, 'navigate');
+    component.signUpUser();
+    expect(component.router.navigate).toHaveBeenCalledTimes(1);
+  });
+
+  it('when retrieve account details fails, should not save account details into session storage, and should show error message', () => {
+    const message = "Invalid Credential";
+    const user = "TESTUSER";
+    const data = {
+      "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJDSE9PTkFOTiIsImV4cCI6MTY3MTA3OTg4NSwiaWF0IjoxNjcwNDc5ODg1fQ.zl_AJFETUvw1WxMjPSgmSb9tTLUjFwg6AHNwS358DQL9kLWs-zYrjG4aPXIWgRlpWM4W0rCx0S0HlFkIJBWfoQ"
+    };
+    spyOn(component.apiService, 'retrieveAccDetails').and.returnValue(of({"statusCode":1,"resultMessage": 'Invalid Credential'}));
+    component.retrieveAccDetails(user, data);
+    expect(component.errorMsg).toBe(message);
+    expect(sessionStorage.getItem(AUTH_USER)).toBeNull();
+    expect(sessionStorage.getItem(TOKEN)).toBeNull();
+    expect(sessionStorage.getItem(ACC_DETAILS)).toBeNull();
+  });
+
+  it('when retrieve account details succeed, should save account details into session storage', () => {
+    const user = "TESTUSER";
+    const data = {
+      "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJDSE9PTkFOTiIsImV4cCI6MTY3MTA3OTg4NSwiaWF0IjoxNjcwNDc5ODg1fQ.zl_AJFETUvw1WxMjPSgmSb9tTLUjFwg6AHNwS358DQL9kLWs-zYrjG4aPXIWgRlpWM4W0rCx0S0HlFkIJBWfoQ"
+    };
+    spyOn(component.apiService, 'retrieveAccDetails').and.returnValue(of({"statusCode":0,"resultMessage": 'Login Successful', "account": new AccountModel()}));
+    component.retrieveAccDetails(user, data);
+    expect(component.errorMsg).toBe(null);
+    expect(sessionStorage.getItem(AUTH_USER)).toBe(user);
+    expect(sessionStorage.getItem(TOKEN)).toBe('Bearer ' + data.token);
+    expect(sessionStorage.getItem(ACC_DETAILS)).not.toBeNull();
+  });
+
+  it('when there is connection issue when retrieving account details, should throw error exception', () => {
+    const error = new HttpErrorResponse({ status: 500 });
+    spyOn(component.apiService, 'retrieveAccDetails').and.returnValue(throwError(error));
+    spyOn(component.sharedVar, 'changeException');
+    const user = "TESTUSER";
+    const data = {
+      "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJDSE9PTkFOTiIsImV4cCI6MTY3MTA3OTg4NSwiaWF0IjoxNjcwNDc5ODg1fQ.zl_AJFETUvw1WxMjPSgmSb9tTLUjFwg6AHNwS358DQL9kLWs-zYrjG4aPXIWgRlpWM4W0rCx0S0HlFkIJBWfoQ"
+    };
+    component.retrieveAccDetails(user, data);
+    expect(component.sharedVar.changeException).toHaveBeenCalledTimes(1);
+  });
+
 });
