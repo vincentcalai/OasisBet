3e7fc74ad7e7c47748d152a86f32f01fc32296b7
Vincent Lai <vincentlai91@hotmail.com>
2023-06-22 13:37:58 +0800

    added unit tests

--- oasisbet-frontend/src/app/odds/odds-landing/odds-landing.component.spec.ts
+++ oasisbet-frontend/src/app/odds/odds-landing/odds-landing.component.spec.ts
@@ -61,7 +61,7 @@ describe('OddsLandingComponent', () => {
     const mockResponse = {
       betEvent: [
         {
-          eventId: 1,
+          eventId: 1000001,
           eventDesc: 'Event 1',
           startTime: '2023-05-15T12:00:00Z',
           competition: 'EPL',
@@ -69,7 +69,7 @@ describe('OddsLandingComponent', () => {
           h2hEventOdds: {}
         },
         {
-          eventId: 2,
+          eventId: 1000002,
           eventDesc: 'Event 2',
           startTime: '2023-05-15T15:30:00Z',
           competition: 'EPL',
@@ -85,6 +85,40 @@ describe('OddsLandingComponent', () => {
     expect(component.eventDates.length).toEqual(1);
     expect(component.eventsMap.size).toEqual(1);
     expect(component.eventsMap.get('Mon May 15 2023').length).toEqual(2);
+  });
+
+  it('should update selected bets based on existing bet slips', () => {
+    const mockResponse = {
+      betEvent: [
+        {
+          eventId: 1000001,
+          eventDesc: 'Event 1',
+          startTime: '2023-05-15T12:00:00Z',
+          competition: 'EPL',
+          teamsDetails: {},
+          h2hEventOdds: {}
+        },
+      ],
+    };
+    spyOn(component.apiService, 'retrieveOdds').and.returnValue(of(mockResponse));
+    const betSlip = {
+      eventId: 1000001,
+      eventDesc: "Leicester City vs Manchester United",
+      compType: "soccer_epl",
+      startTime: new Date(),
+      betSelection: "01",
+      betSelectionName: 'Leicester City',
+      betTypeCd: "01",
+      odds: 5.00,
+      betAmount: 5.00,
+      potentialPayout: 25.00
+    };
+    component.selectedBets.push(betSlip);
+    component.ngOnInit();
+    console.log("events:", component.events[0].betSelection);
+    expect(component.apiService.retrieveOdds).toHaveBeenCalledWith(component.compType);
+    expect(component.events[0].betSelection.homeSelected).toBeTruthy();
+    expect(component.selectedBets).toEqual([betSlip]);
   });
 
 });
