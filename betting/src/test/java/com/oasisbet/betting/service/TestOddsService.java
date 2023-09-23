18fda85f6184e6a9218bbee379a01a113e28ed06
Vincent Lai <vincentlai91@hotmail.com>
2023-09-23 15:15:41 +0800

    added unit tests on retrieve bet events

--- betting/src/test/java/com/oasisbet/betting/service/TestOddsService.java
+++ betting/src/test/java/com/oasisbet/betting/service/TestOddsService.java
@@ -2,6 +2,7 @@ package com.oasisbet.betting.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigInteger;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -25,5 +26,56 @@ class TestOddsService extends TestBaseSetup {
 	void testRetrieveBetEventByCompTypeCountIs3() {
 		List<BetEvent> results = oddsService.retrieveBetEventByCompType("soccer_epl");
 		assertEquals(3, results.size());
+	}
+
+	@Test
+	void testRetrieveBetEventByCompTypeCheckTeamNameSuccess() {
+		List<BetEvent> results = oddsService.retrieveBetEventByCompType("soccer_epl");
+		String homeTeam1 = results.get(0).getTeamsDetails().getHomeTeam();
+		String awayTeam1 = results.get(0).getTeamsDetails().getAwayTeam();
+		String homeTeam2 = results.get(1).getTeamsDetails().getHomeTeam();
+		String awayTeam2 = results.get(1).getTeamsDetails().getAwayTeam();
+		String homeTeam3 = results.get(2).getTeamsDetails().getHomeTeam();
+		String awayTeam3 = results.get(2).getTeamsDetails().getAwayTeam();
+
+		assertEquals("Tottenham Hotspur", homeTeam1);
+		assertEquals("Manchester United", awayTeam1);
+		assertEquals("Southampton", homeTeam2);
+		assertEquals("Bournemouth", awayTeam2);
+		assertEquals("Everton", homeTeam3);
+		assertEquals("Newcastle United", awayTeam3);
+	}
+
+	@Test
+	void testRetrieveBetEventByCompTypeCheckOddsSuccess() {
+		List<BetEvent> results = oddsService.retrieveBetEventByCompType("soccer_epl");
+		double homeOdds1 = results.get(0).getH2hEventOdds().getHomeOdds();
+		double drawOdds1 = results.get(0).getH2hEventOdds().getDrawOdds();
+		double awayOdds1 = results.get(0).getH2hEventOdds().getAwayOdds();
+		double homeOdds2 = results.get(1).getH2hEventOdds().getHomeOdds();
+		double drawOdds2 = results.get(1).getH2hEventOdds().getDrawOdds();
+		double awayOdds2 = results.get(1).getH2hEventOdds().getAwayOdds();
+		double homeOdds3 = results.get(2).getH2hEventOdds().getHomeOdds();
+		double drawOdds3 = results.get(2).getH2hEventOdds().getDrawOdds();
+		double awayOdds3 = results.get(2).getH2hEventOdds().getAwayOdds();
+
+		assertEquals(2.81, homeOdds1);
+		assertEquals(3.71, drawOdds1);
+		assertEquals(2.49, awayOdds1);
+		assertEquals(2.32, homeOdds2);
+		assertEquals(3.46, drawOdds2);
+		assertEquals(3.25, awayOdds2);
+		assertEquals(5.17, homeOdds3);
+		assertEquals(3.85, drawOdds3);
+		assertEquals(1.74, awayOdds3);
+	}
+
+	@Test
+	void testRetrieveBetEventSequenceOfCommenceTimeSuccess() {
+		List<BetEvent> results = oddsService.retrieveBetEventByCompType("soccer_epl");
+
+		assertEquals(BigInteger.valueOf(1000000), results.get(0).getEventId());
+		assertEquals(BigInteger.valueOf(1000001), results.get(1).getEventId());
+		assertEquals(BigInteger.valueOf(1000002), results.get(2).getEventId());
 	}
 }
