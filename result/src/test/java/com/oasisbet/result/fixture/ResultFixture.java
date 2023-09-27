2841096664a8645543e22461f4fbf8fecea1e006
Vincent Lai <vincentlai91@hotmail.com>
2023-09-27 11:10:33 +0800

    added unit test for result service

--- result/src/test/java/com/oasisbet/result/fixture/ResultFixture.java
+++ result/src/test/java/com/oasisbet/result/fixture/ResultFixture.java
@@ -139,4 +139,110 @@ public class ResultFixture {
 		return resultEvents;
 	}
 
+	public static ResultApiResponse[] mockEplResultApiResponseArrayHomeAndAwayScoreSwapped() {
+
+		ResultApiResponse[] array = new ResultApiResponse[3];
+
+		ResultApiResponse mockResponse1 = new ResultApiResponse();
+		ResultApiResponse mockResponse2 = new ResultApiResponse();
+		ResultApiResponse mockResponse3 = new ResultApiResponse();
+
+		List<Score> scoreList1 = new ArrayList<>();
+		Score mockHomeScore1 = new Score();
+		mockHomeScore1.setName("Tottenham Hotspur");
+		mockHomeScore1.setScore("4");
+		scoreList1.add(mockHomeScore1);
+		Score mockAwayScore1 = new Score();
+		mockAwayScore1.setName("Leicester City");
+		mockAwayScore1.setScore("1");
+		scoreList1.add(mockAwayScore1);
+
+		mockResponse1.setId("a085aa8beb661722ad957e5d8c15f798");
+		mockResponse1.setSport_key("soccer_epl");
+		mockResponse1.setSport_title("English Premier League");
+		mockResponse1.setCommence_time("2023-04-28T18:45:00Z");
+		mockResponse1.setHome_team("Tottenham Hotspur");
+		mockResponse1.setAway_team("Leicester City");
+		mockResponse1.setCompleted(true);
+		mockResponse1.setLast_update("2023-04-29T18:45:00Z");
+		mockResponse1.setScores(scoreList1);
+
+		List<Score> scoreList2 = new ArrayList<>();
+		Score mockHomeScore2 = new Score();
+		mockHomeScore2.setName("Sheffield United");
+		mockHomeScore2.setScore("0");
+		scoreList2.add(mockHomeScore2);
+		Score mockAwayScore2 = new Score();
+		mockAwayScore2.setName("Arsenal");
+		mockAwayScore2.setScore("4");
+		scoreList2.add(mockAwayScore2);
+
+		mockResponse2.setId("f7d5d5a141e21df15f23b5e306340bed");
+		mockResponse2.setSport_key("soccer_epl");
+		mockResponse2.setSport_title("English Premier League");
+		mockResponse2.setCommence_time("2023-04-29T18:45:00Z");
+		mockResponse2.setHome_team("Arsenal");
+		mockResponse2.setAway_team("Sheffield United");
+		mockResponse2.setCompleted(true);
+		mockResponse2.setLast_update("2023-04-30T18:45:00Z");
+		mockResponse2.setScores(scoreList2);
+
+		List<Score> scoreList3 = new ArrayList<>();
+		Score mockHomeScore3 = new Score();
+		mockHomeScore3.setName("Manchester United");
+		mockHomeScore3.setScore("1");
+		scoreList3.add(mockHomeScore3);
+		Score mockAwayScore3 = new Score();
+		mockAwayScore3.setName("Newcastle United");
+		mockAwayScore3.setScore("1");
+		scoreList3.add(mockAwayScore3);
+
+		mockResponse3.setId("66ca5a121b5ddc4763cf1708222be377");
+		mockResponse3.setSport_key("soccer_epl");
+		mockResponse3.setSport_title("English Premier League");
+		mockResponse3.setCommence_time("2023-04-30T19:45:00Z");
+		mockResponse3.setHome_team("Newcastle United");
+		mockResponse3.setAway_team("Manchester United");
+		mockResponse3.setCompleted(true);
+		mockResponse3.setLast_update("2023-04-30T22:45:00Z");
+		mockResponse3.setScores(scoreList3);
+
+		array[0] = mockResponse1;
+		array[1] = mockResponse2;
+		array[2] = mockResponse3;
+
+		return array;
+	}
+
+	public static ResultApiResponse[] mockEplResultApiResponseArrayWithDateFormatError() {
+
+		ResultApiResponse[] array = new ResultApiResponse[1];
+
+		ResultApiResponse mockResponse1 = new ResultApiResponse();
+
+		List<Score> scoreList1 = new ArrayList<>();
+		Score mockHomeScore1 = new Score();
+		mockHomeScore1.setName("Tottenham Hotspur");
+		mockHomeScore1.setScore("4");
+		scoreList1.add(mockHomeScore1);
+		Score mockAwayScore1 = new Score();
+		mockAwayScore1.setName("Leicester City");
+		mockAwayScore1.setScore("1");
+		scoreList1.add(mockAwayScore1);
+
+		mockResponse1.setId("a085aa8beb661722ad957e5d8c15f798");
+		mockResponse1.setSport_key("soccer_epl");
+		mockResponse1.setSport_title("English Premier League");
+		mockResponse1.setCommence_time("ERROR FORMAT");
+		mockResponse1.setHome_team("Tottenham Hotspur");
+		mockResponse1.setAway_team("Leicester City");
+		mockResponse1.setCompleted(true);
+		mockResponse1.setLast_update("2023-04-29T18:45:00Z");
+		mockResponse1.setScores(scoreList1);
+
+		array[0] = mockResponse1;
+
+		return array;
+	}
+
 }
