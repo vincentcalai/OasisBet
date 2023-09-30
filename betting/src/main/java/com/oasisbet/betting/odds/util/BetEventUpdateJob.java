87fa6ffc4b4f6cf65ae795996087b41c796fae40
Vincent Lai <vincentlai91@hotmail.com>
2023-09-30 10:47:24 +0800

    added test cases in TestResultUpdateJob

--- betting/src/main/java/com/oasisbet/betting/odds/util/BetEventUpdateJob.java
+++ betting/src/main/java/com/oasisbet/betting/odds/util/BetEventUpdateJob.java
@@ -6,8 +6,10 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import com.oasisbet.betting.odds.model.response.OddsApiResponse;
 import com.oasisbet.betting.odds.service.OddsService;
@@ -16,7 +18,10 @@ import com.oasisbet.betting.odds.service.OddsService;
 public class BetEventUpdateJob implements Job {
 
 	@Autowired
-	public OddsService oddsService;
+	private OddsService oddsService;
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
@@ -25,31 +30,30 @@ public class BetEventUpdateJob implements Job {
 		log.info("executing BetEventUpdateJob...");
 
 		for (String compType : Constants.API_SOURCE_COMP_TYPE_LIST) {
-//			String uri = Constants.API_SOURCE_BASE_URI + compType + Constants.API_SOURCE_URI_ODDS + Constants.API_SOURCE_URI_API_KEY_PARAM
-//					+ Constants.API_SOURCE_API_KEY + Constants.AMPERSAND + Constants.API_SOURCE_URI_REGION_PARAM
-//					+ Constants.API_SOURCE_URI_EU_REGION + Constants.AMPERSAND + Constants.API_SOURCE_URI_MARKET_PARAM
-//					+ Constants.API_SOURCE_URI_MARKET_H2H + Constants.AMPERSAND
-//					+ Constants.API_SOURCE_URI_DATE_FORMAT_PARAM + Constants.API_SOURCE_URI_DATE_FORMAT_ISO
-//					+ Constants.AMPERSAND + Constants.API_SOURCE_URI_ODDS_FORMAT_PARAM
-//					+ Constants.API_SOURCE_URI_ODDS_FORMAT_DEC + Constants.AMPERSAND
-//					+ Constants.API_SOURCE_URI_BOOKMKR_PARAM + Constants.API_SOURCE_URI_BOOKMKR_PINNACLE;
-//			RestTemplate restTemplate = new RestTemplate();
+			String uri = Constants.API_SOURCE_BASE_URI + compType + Constants.API_SOURCE_URI_ODDS
+					+ Constants.API_SOURCE_URI_API_KEY_PARAM + Constants.API_SOURCE_API_KEY + Constants.AMPERSAND
+					+ Constants.API_SOURCE_URI_REGION_PARAM + Constants.API_SOURCE_URI_EU_REGION + Constants.AMPERSAND
+					+ Constants.API_SOURCE_URI_MARKET_PARAM + Constants.API_SOURCE_URI_MARKET_H2H + Constants.AMPERSAND
+					+ Constants.API_SOURCE_URI_DATE_FORMAT_PARAM + Constants.API_SOURCE_URI_DATE_FORMAT_ISO
+					+ Constants.AMPERSAND + Constants.API_SOURCE_URI_ODDS_FORMAT_PARAM
+					+ Constants.API_SOURCE_URI_ODDS_FORMAT_DEC + Constants.AMPERSAND
+					+ Constants.API_SOURCE_URI_BOOKMKR_PARAM + Constants.API_SOURCE_URI_BOOKMKR_PINNACLE;
 			OddsApiResponse[] results = null;
 			try {
-//				ResponseEntity<OddsApiResponse[]> responseEntity = restTemplate.getForEntity(uri,
-//						OddsApiResponse[].class);
-//				results = responseEntity.getBody();
-				if (compType.equals(Constants.API_SOURCE_COMP_TYPE_EPL)) {
-					results = MockData.mockEplOddsApiResponseArray();
-				} else if (compType.equals(Constants.API_SOURCE_COMP_TYPE_LALIGA)) {
-					results = MockData.mockLaLigaOddsApiResponseArray();
-				} else if (compType.equals(Constants.API_SOURCE_COMP_TYPE_BUNDESLIGA)) {
-					results = MockData.mockBundesligaOddsApiResponseArray();
-				} else if (compType.equals(Constants.API_SOURCE_COMP_TYPE_SERIE_A)) {
-					results = MockData.mockSerieAOddsApiResponseArray();
-				} else if (compType.equals(Constants.API_SOURCE_COMP_TYPE_LIGUE_ONE)) {
-					results = MockData.mockLigueOneOddsApiResponseArray();
-				}
+				ResponseEntity<OddsApiResponse[]> responseEntity = restTemplate.getForEntity(uri,
+						OddsApiResponse[].class);
+				results = responseEntity.getBody();
+//				if (compType.equals(Constants.API_SOURCE_COMP_TYPE_EPL)) {
+//					results = MockData.mockEplOddsApiResponseArray();
+//				} else if (compType.equals(Constants.API_SOURCE_COMP_TYPE_LALIGA)) {
+//					results = MockData.mockLaLigaOddsApiResponseArray();
+//				} else if (compType.equals(Constants.API_SOURCE_COMP_TYPE_BUNDESLIGA)) {
+//					results = MockData.mockBundesligaOddsApiResponseArray();
+//				} else if (compType.equals(Constants.API_SOURCE_COMP_TYPE_SERIE_A)) {
+//					results = MockData.mockSerieAOddsApiResponseArray();
+//				} else if (compType.equals(Constants.API_SOURCE_COMP_TYPE_LIGUE_ONE)) {
+//					results = MockData.mockLigueOneOddsApiResponseArray();
+//				}
 
 				// add new bet events to sports_event_mapping, generate new event id for new bet
 				// events
--- result/src/test/java/com/oasisbet/result/job/TestResultUpdateJob.java
+++ result/src/test/java/com/oasisbet/result/job/TestResultUpdateJob.java
@@ -1,83 +1,117 @@
 package com.oasisbet.result.job;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+
+import java.math.BigInteger;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.JobKey;
-import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
-import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.oasisbet.result.TestBaseSetup;
+import com.oasisbet.result.dao.IResultEventMappingDao;
 import com.oasisbet.result.fixture.ResultFixture;
 import com.oasisbet.result.model.ResultApiResponse;
-import com.oasisbet.result.util.ResultHouseKeepingJob;
+import com.oasisbet.result.model.ResultEventMapping;
 import com.oasisbet.result.util.ResultUpdateJob;
 
 @ExtendWith(MockitoExtension.class)
 class TestResultUpdateJob extends TestBaseSetup {
 
 	@MockBean
-	private RestTemplate mockRestTemplate;
+	private RestTemplate restTemplate;
 
-	public JobDetail testResultUpdateJobDetail() {
-		return JobBuilder.newJob(ResultUpdateJob.class).withIdentity("resultUpdateJob", "testResultGroup")
-				.storeDurably().build();
-	}
+	@Autowired
+	private ResultUpdateJob resultUpdateJob;
 
-	public JobDetail testResultHouseKeepingJobDetail() {
-		return JobBuilder.newJob(ResultHouseKeepingJob.class).withIdentity("resultHouseKeepingJob", "testResultGroup")
-				.storeDurably().build();
-	}
+	@Autowired
+	private IResultEventMappingDao resultEventMappingDao;
 
 	@Test
-	void testResultUpdateJob() throws SchedulerException, InterruptedException {
+	void testResultUpdateJob3NewEventsInsertedSuccess() throws SchedulerException, InterruptedException {
 
-		Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+		List<ResultEventMapping> resultEventList = resultEventMappingDao.findAll();
+		assertEquals(23, resultEventList.size());
 
-		Trigger trigger = TriggerBuilder.newTrigger().withIdentity("testResultUpdateJobTrigger").startNow().build();
-
-		scheduler.scheduleJob(testResultUpdateJobDetail(), trigger);
-
-		scheduler.start();
 		ResultApiResponse[] mockBody = ResultFixture.mockEplResultApiResponseArray();
-		ResponseEntity<Object> mockResponseEntity = new ResponseEntity<>(mockBody, HttpStatus.OK);
 
-		when(mockRestTemplate.getForEntity(Mockito.anyString(), Mockito.any()))
-				.thenAnswer(invocation -> mockResponseEntity);
+		when(restTemplate.getForEntity(Mockito.anyString(), Mockito.eq(ResultApiResponse[].class)))
+				.thenReturn(new ResponseEntity<>(mockBody, HttpStatus.OK));
 
-		JobKey jobKey = new JobKey("resultUpdateJob", "testResultGroup");
+		resultUpdateJob.execute(null);
 
-		scheduler.triggerJob(jobKey);
-		scheduler.shutdown(true);
+		List<ResultEventMapping> newResultEventList = resultEventMappingDao.findAll();
+		assertEquals(26, newResultEventList.size());
 	}
 
 	@Test
-	void testResultHouseKeepingJob() throws SchedulerException, InterruptedException {
-		Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+	void testResultUpdateJob3NewEventsInsertedVerifyApiEventIdSuccess()
+			throws SchedulerException, InterruptedException {
 
-		Trigger trigger = TriggerBuilder.newTrigger().withIdentity("testResultHouseKeepingJobTrigger").startNow()
-				.build();
+		ResultApiResponse[] mockBody = ResultFixture.mockEplResultApiResponseArray();
 
-		scheduler.scheduleJob(testResultHouseKeepingJobDetail(), trigger);
+		when(restTemplate.getForEntity(Mockito.anyString(), Mockito.eq(ResultApiResponse[].class)))
+				.thenReturn(new ResponseEntity<>(mockBody, HttpStatus.OK));
 
-		scheduler.start();
+		resultUpdateJob.execute(null);
 
-		JobKey jobKey = new JobKey("resultHouseKeepingJob", "testResultGroup");
-		scheduler.triggerJob(jobKey);
+		List<ResultEventMapping> newResultEventList = resultEventMappingDao.findAll();
+		long eventCount1 = newResultEventList.stream()
+				.filter(event -> event.getApiEventId().equals("e306340bed661722ad957e5d8c15f798")).count();
+		long eventCount2 = newResultEventList.stream()
+				.filter(event -> event.getApiEventId().equals("2ad957e5d8661722ad957e5d8c15f798")).count();
+		long eventCount3 = newResultEventList.stream()
+				.filter(event -> event.getApiEventId().equals("1722ad957e661722ad957e5d8c15f798")).count();
+		assertEquals(1, eventCount1);
+		assertEquals(1, eventCount2);
+		assertEquals(1, eventCount3);
+	}
 
-		scheduler.shutdown(true);
+	@Test
+	void testResultUpdateJob3NewEventsInsertedVerifyEventIdSuccess() throws SchedulerException, InterruptedException {
+
+		ResultApiResponse[] mockBody = ResultFixture.mockEplResultApiResponseArray();
+
+		when(restTemplate.getForEntity(Mockito.anyString(), Mockito.eq(ResultApiResponse[].class)))
+				.thenReturn(new ResponseEntity<>(mockBody, HttpStatus.OK));
+
+		resultUpdateJob.execute(null);
+
+		List<ResultEventMapping> newResultEventList = resultEventMappingDao.findAll();
+		long eventCount1 = newResultEventList.stream()
+				.filter(event -> event.getEventId().equals(BigInteger.valueOf(1000003L))).count();
+		long eventCount2 = newResultEventList.stream()
+				.filter(event -> event.getEventId().equals(BigInteger.valueOf(1000004L))).count();
+		long eventCount3 = newResultEventList.stream()
+				.filter(event -> event.getEventId().equals(BigInteger.valueOf(1000005L))).count();
+		assertEquals(1, eventCount1);
+		assertEquals(1, eventCount2);
+		assertEquals(1, eventCount3);
+	}
+
+	@Test
+	void testResultUpdateJobConnFail3NewEventsNotInserted() throws SchedulerException, InterruptedException {
+
+		List<ResultEventMapping> resultEventList = resultEventMappingDao.findAll();
+		assertEquals(23, resultEventList.size());
+
+		when(restTemplate.getForEntity(Mockito.anyString(), Mockito.eq(ResultApiResponse[].class)))
+				.thenThrow(RestClientException.class);
+
+		resultUpdateJob.execute(null);
+
+		List<ResultEventMapping> newResultEventList = resultEventMappingDao.findAll();
+		assertEquals(23, newResultEventList.size());
 	}
 
 }
