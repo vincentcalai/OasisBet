df2575dda0329f5c3dc01fb23a80485aa6a534fa
Vincent Lai <vincentlai91@hotmail.com>
2023-11-24 22:10:32 +0800

    added details to set authorisation headers in interceptor

--- oasisbet-frontend/src/app/services/api/interceptor.module.ts
+++ oasisbet-frontend/src/app/services/api/interceptor.module.ts
@@ -1,17 +1,25 @@
 import { Injectable, NgModule} from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS} from '@angular/common/http';
-import { SharedVarService } from '../shared-var.service';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS, HttpHeaders} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class HttpsRequestInterceptor implements HttpInterceptor {
 
   constructor(
-    public sharedVar: SharedVarService
+
   ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req);
+    let httpHeaders = new HttpHeaders();
+    let authorization = sessionStorage.getItem('Authorization');
+    if(authorization){
+      httpHeaders = httpHeaders.append('Authorization', authorization);
+    }
+    const xhr = req.clone({
+      headers: httpHeaders
+    });
+    return next.handle(xhr);
   }
 }
 
