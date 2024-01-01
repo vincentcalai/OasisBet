7af55644b811c1a5f606a58a75a9efa0c8cdfc75
Vincent Lai <vincentlai91@hotmail.com>
2024-01-01 15:41:32 +0800

    added login logic for main menu login

--- oasisbet-frontend/src/app/views/c-main-menu/main-menu.component.ts
+++ oasisbet-frontend/src/app/views/c-main-menu/main-menu.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AuthService, LOGIN_TIME } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { Subscription, throwError } from 'rxjs';
+import { take, finalize, switchMap, catchError } from 'rxjs/operators';
+import { ApiService } from 'src/app/services/api/api.service';
+import { ACC_DETAILS, AUTHORIZATION, AUTH_USER, AuthService, LOGIN_TIME } from 'src/app/services/auth/auth.service';
 import { SharedMethodsService } from 'src/app/services/shared-methods.service';
 import { SharedVarService } from 'src/app/services/shared-var.service';
 
@@ -12,10 +15,17 @@ import { SharedVarService } from 'src/app/services/shared-var.service';
 export class MainMenuComponent implements OnInit {
 
   public spinme: string = 'none';
+  username: string = "";
+  password: string = "";
+  token: string = "";
+  errorMsg: string = "";
+  responseMsg: string = "";
 
   public subscriptions: Subscription = new Subscription();
 
   constructor(public authService: AuthService,
+    public router: Router,
+    public apiService: ApiService, 
     public sharedVar: SharedVarService,
     public sharedMethods: SharedMethodsService) { }
 
@@ -27,6 +37,51 @@ export class MainMenuComponent implements OnInit {
     this.subscriptions.add(
       this.sharedVar.currentSpinner.subscribe(status => this.spinme = status)
     );
+  }
+
+  handleJWTAuthLogin(){
+    this.responseMsg = "";
+    this.sharedVar.changeSpinner('block');
+    this.sharedVar.loginCredentialsModel.username = this.username;
+    this.sharedVar.loginCredentialsModel.password = this.password;
+    this.authService.jwtAuthenticate()
+    .pipe(
+      take(1),
+      finalize(() => this.sharedVar.changeSpinner('none')),
+      switchMap(
+        (data: any) => {
+          this.token = data.token;
+          sessionStorage.setItem(AUTH_USER, this.username);
+          sessionStorage.setItem(AUTHORIZATION, `Bearer ${this.token}`);
+          sessionStorage.setItem(LOGIN_TIME, Date.now().toString());
+          this.authService.startLoginTimer();
+          return this.apiService.retrieveAccDetails(this.username)
+        }
+      ),
+      catchError((error) => {
+        console.error('Authentication error:', error);
+        return throwError('Authentication failed');
+      })
+    )
+    .subscribe(
+        (resp: any) => {
+          console.log(resp);
+          if (resp.statusCode !== 0) {
+            this.errorMsg = resp.resultMessage;
+          } else {
+            sessionStorage.setItem(ACC_DETAILS, JSON.stringify(resp.account));
+            console.log("login successful");
+          }
+        },
+        (error) => {
+          console.log("login fail", error);
+          this.errorMsg = this.sharedVar.INVALID_LOGIN_ERR_MSG;
+        }
+    )
+  }
+
+  signUpUser(){
+    this.router.navigate(['create-user'], {skipLocationChange: true});
   }
 
   logout(): void {
