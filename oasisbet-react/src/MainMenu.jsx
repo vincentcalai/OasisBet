f1f102a3b8009d837c496f23899ba03d8f48cbfc
Vincent Lai <vincentlai91@hotmail.com>
2024-04-01 22:53:06 +0800

    added global css files added main menu UI components

--- oasisbet-react/src/MainMenu.jsx
+++ oasisbet-react/src/MainMenu.jsx
@@ -1,6 +1,43 @@
+import './MainMenu.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+import Button from 'react-bootstrap/Button';
+
 
 export default function MainMenu(){
     return (
-        <p>MainMenu Works!</p>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link main-menu-link" routerLink="/odds" routerLinkActive="active">Odds</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link main-menu-link" routerLink="/results" routerLinkActive="active">Results</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link main-menu-link" routerLink="/account" routerLinkActive="active">Account</a>
+                    </li>
+                    </ul>
+                </div>
+
+                <div class="right-navbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item user-menu-display">
+                            <span>LOGGED IN &nbsp;</span>
+                            <Button type="button" variant="secondary" class="btn-logout">Logout</Button>
+                            <FontAwesomeIcon icon={faUser} class="user-icon"/>
+                            <a class="login-username-tag" routerLink="/account" routerLinkActive="active"> CHOONANN</a>
+                        </li>
+                        <li class="nav-item">
+                            <span class="balance-textbox">Balance: $99.88</span>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            
+        </header>
     );
 }
--- oasisbet-react/src/index.js
+++ oasisbet-react/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
