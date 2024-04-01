5842f55fe69edd9e51fb5a45eb03c5185ebf8ba5
Vincent Lai <vincentlai91@hotmail.com>
2024-04-01 23:03:04 +0800

    amended all class to className to accomodate react class term naming
    Added css for Main Menu button

--- oasisbet-react/src/MainMenu.jsx
+++ oasisbet-react/src/MainMenu.jsx
@@ -7,31 +7,31 @@ import Button from 'react-bootstrap/Button';
 export default function MainMenu(){
     return (
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link main-menu-link" routerLink="/odds" routerLinkActive="active">Odds</a>
+            <nav className ="navbar navbar-expand-lg navbar-light">
+                <div className ="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className ="navbar-nav">
+                    <li className ="nav-item">
+                        <a className ="nav-link main-menu-link" routerLink="/odds" routerLinkActive="active">Odds</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link main-menu-link" routerLink="/results" routerLinkActive="active">Results</a>
+                    <li className ="nav-item">
+                        <a className ="nav-link main-menu-link" routerLink="/results" routerLinkActive="active">Results</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link main-menu-link" routerLink="/account" routerLinkActive="active">Account</a>
+                    <li className ="nav-item">
+                        <a className ="nav-link main-menu-link" routerLink="/account" routerLinkActive="active">Account</a>
                     </li>
                     </ul>
                 </div>
 
-                <div class="right-navbar">
-                    <ul class="navbar-nav">
-                        <li class="nav-item user-menu-display">
+                <div className ="right-navbar">
+                    <ul className ="navbar-nav">
+                        <li className ="nav-item user-menu-display">
                             <span>LOGGED IN &nbsp;</span>
-                            <Button type="button" variant="secondary" class="btn-logout">Logout</Button>
-                            <FontAwesomeIcon icon={faUser} class="user-icon"/>
-                            <a class="login-username-tag" routerLink="/account" routerLinkActive="active"> CHOONANN</a>
+                            <Button type="button" variant="secondary" className ="btn-logout">Logout</Button>
+                            <FontAwesomeIcon icon={faUser} className ="user-icon"/>
+                            <a className ="login-username-tag" routerLink="/account" routerLinkActive="active"> CHOONANN</a>
                         </li>
-                        <li class="nav-item">
-                            <span class="balance-textbox">Balance: $99.88</span>
+                        <li className ="nav-item">
+                            <span className ="balance-textbox">Balance: $99.88</span>
                         </li>
                     </ul>
                 </div>
