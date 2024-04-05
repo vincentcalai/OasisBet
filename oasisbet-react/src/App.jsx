3ebb1665efbfa44fa7d65f716a5ca53bdf577c49
Vincent Lai <vincentlai91@hotmail.com>
2024-04-05 22:02:26 +0800

    added global css file added highlight side nav css when selected state

--- oasisbet-react/src/App.jsx
+++ oasisbet-react/src/App.jsx
@@ -3,13 +3,22 @@ import Header from './component/Header.jsx';
 import MainMenu from './component/MainMenu.jsx';
 import CompSideNav from './component/CompSideNav.jsx';
 import SharedVarConstants from './constants/SharedVarConstants.js';
+import {useState} from 'react';
 
 function App() {
+
+  const [compType, setCompType] = useState(null);
+
+  const selectCompType = (newCompType) => {
+      setCompType(newCompType);
+  };
+
   return (
     <div className="App">
       <Header></Header>
       <MainMenu></MainMenu>
-      <CompSideNav sharedVar={SharedVarConstants}></CompSideNav>
+      <CompSideNav compType={compType} sharedVar={SharedVarConstants} selectCompType={selectCompType}></CompSideNav>
+      {compType}
     </div>
   );
 }
