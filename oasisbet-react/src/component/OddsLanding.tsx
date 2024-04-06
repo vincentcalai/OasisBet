5abc2b5dc3908607b74d901c2344a2ba4c4ca1f1
Vincent Lai <vincentlai91@hotmail.com>
2024-04-06 08:24:30 +0800

    added state to retrieve Competiton Header

--- oasisbet-react/src/component/OddsLanding.tsx
+++ oasisbet-react/src/component/OddsLanding.tsx
@@ -7,14 +7,14 @@ import React from 'react';
 
 
 export default function OddsLanding({sharedVar}){
-
+    
     const [compType, setCompType] = useState(null);
+    const [compTypeHdr, setCompTypeHdr] = useState(sharedVar.COMP_HEADER_EPL);
 
     const selectCompType = (newCompType) => {
+        setCompTypeHdr(retrieveCompHdr(sharedVar, newCompType));
         setCompType(newCompType);
     };
-
-    let competitionTypeHdr: string = sharedVar.COMP_HEADER_EPL;
 
     return (
         <>
@@ -26,9 +26,9 @@ export default function OddsLanding({sharedVar}){
                     <div className="col-7">
                         <Card className="card">
                             <Card.Header className="card-header">
-                                <p>{competitionTypeHdr}</p>
+                                <h2>{compTypeHdr}</h2>
                             </Card.Header>
-                            <Card.Body>
+                            <Card.Body className="card-body">
                                 <p>{compType}</p>
                             </Card.Body>
                         </Card>
@@ -41,4 +41,36 @@ export default function OddsLanding({sharedVar}){
             
         </>
     );
+}
+
+function retrieveCompHdr(sharedVar, newCompType): string {
+    switch(newCompType) { 
+        case sharedVar.API_SOURCE_COMP_TYPE_EPL: { 
+            return sharedVar.COMP_HEADER_EPL; 
+        } 
+        case sharedVar.API_SOURCE_COMP_TYPE_LALIGA: { 
+            return sharedVar.COMP_HEADER_LALIGA; 
+        } 
+        case sharedVar.API_SOURCE_COMP_TYPE_BUNDESLIGA: {
+            return sharedVar.COMP_HEADER_BUNDESLIGA; 
+        }
+        case sharedVar.API_SOURCE_COMP_TYPE_SERIE_A: {
+            return sharedVar.COMP_HEADER_SERIE_A; 
+        }
+        case sharedVar.API_SOURCE_COMP_TYPE_LIGUE_ONE: {
+            return sharedVar.COMP_HEADER_LIGUE_ONE; 
+        }
+        case sharedVar.API_SOURCE_COMP_TYPE_FA_CUP: {
+            return sharedVar.COMP_HEADER_FA_CUP; 
+        }
+        case sharedVar.API_SOURCE_COMP_TYPE_EFL_CUP: {
+            return sharedVar.COMP_HEADER_EFL_CUP; 
+        }
+        case sharedVar.API_SOURCE_COMP_TYPE_UCL: {
+            return sharedVar.COMP_HEADER_UCL; 
+        }
+        default: {
+            return '';
+        }
+    }
 }
