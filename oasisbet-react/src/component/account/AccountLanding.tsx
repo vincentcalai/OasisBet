de062a4b15612632dd4e71e378c8c03275e2c9af
Vincent Lai <vincentlai91@hotmail.com>
2024-05-19 13:52:27 +0800

    added logic to bind clicking event to render header on screen for Account Landing page

--- oasisbet-react/src/component/account/AccountLanding.tsx
+++ oasisbet-react/src/component/account/AccountLanding.tsx
@@ -1,11 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import './AccountLanding.css';
 import AccountLogin from "./AccountLogin.tsx";
+import AccountSideNav from "../common/AccountSideNav.tsx";
+import SharedVarConstants from "../../constants/SharedVarConstants.js";
+import { Card } from "react-bootstrap";
 
 export default function AccountLanding(){
+
+    const isUserLoggedIn = false;
+
+    const [accountMenuSelect, setAccountMenuSelect] = useState(SharedVarConstants.NAV_MENU_SELECT_ACCOUNT_OVERVIEW);
+    const [accountMenuHdr, setAccountMenuHdr] = useState(SharedVarConstants.ACCOUNT_OVERVIEW_HEADER);
+
+    const onHandleAccountMenuSelection = (menuSelect) => {
+        setAccountMenuHdr(retrieveAccountMenuHdr(SharedVarConstants, accountMenuSelect));
+        setAccountMenuSelect(menuSelect);
+    };
+
+    function retrieveAccountMenuHdr(sharedVar, accountMenuSelect): string {
+        switch(accountMenuSelect) { 
+            case sharedVar.API_SOURCE_COMP_TYPE_EPL: { 
+                return sharedVar.COMP_HEADER_EPL; 
+            } 
+            case sharedVar.API_SOURCE_COMP_TYPE_LALIGA: { 
+                return sharedVar.COMP_HEADER_LALIGA; 
+            } 
+            case sharedVar.API_SOURCE_COMP_TYPE_BUNDESLIGA: {
+                return sharedVar.COMP_HEADER_BUNDESLIGA; 
+            }
+            case sharedVar.API_SOURCE_COMP_TYPE_SERIE_A: {
+                return sharedVar.COMP_HEADER_SERIE_A; 
+            }
+            case sharedVar.API_SOURCE_COMP_TYPE_LIGUE_ONE: {
+                return sharedVar.COMP_HEADER_LIGUE_ONE; 
+            }
+            case sharedVar.API_SOURCE_COMP_TYPE_FA_CUP: {
+                return sharedVar.COMP_HEADER_FA_CUP; 
+            }
+            case sharedVar.API_SOURCE_COMP_TYPE_EFL_CUP: {
+                return sharedVar.COMP_HEADER_EFL_CUP; 
+            }
+            case sharedVar.API_SOURCE_COMP_TYPE_UCL: {
+                return sharedVar.COMP_HEADER_UCL; 
+            }
+            default: {
+                return '';
+            }
+        }
+    }
+    
+
     return (
         <>
-            <AccountLogin></AccountLogin>
+            {!isUserLoggedIn && <AccountLogin></AccountLogin>}
+
+            {isUserLoggedIn && 
+                <div className="container">
+                    <div className="row">
+                        <div className="col-2">
+                            <AccountSideNav accountMenuSelect={accountMenuSelect} handleClick={onHandleAccountMenuSelection}>
+                            </AccountSideNav>
+                        </div>
+                        <div className="col-10">
+                            <div className="container-fluid">
+                                <Card className="card" style={{tableLayout: 'fixed', width: '100%', marginLeft: '30px' }}>
+                                    <Card.Header className="card-header">
+                                        <h2>{accountMenuHdr}</h2>
+                                    </Card.Header>
+                                    <Card.Body className="card-body">
+                                        
+                                    </Card.Body> 
+                                </Card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
         </>
     );
 }
---
+++ oasisbet-react/src/component/common/AccountSideNav.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CompSideNav.css';
+import SharedVarConstants from '../../constants/SharedVarConstants';
+
+function AccountSideNav({ accountMenuSelect, handleClick }) {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li className={`sidebar-item ${accountMenuSelect === SharedVarConstants.NAV_MENU_SELECT_ACCOUNT_OVERVIEW ? 'highlightSideNav' : ''}`} onClick={() => handleClick(SharedVarConstants.NAV_MENU_SELECT_ACCOUNT_OVERVIEW)}>{SharedVarConstants.ACCOUNT_OVERVIEW_HEADER}</li>
+                <li className={`sidebar-item ${accountMenuSelect === SharedVarConstants.NAV_MENU_SELECT_ACCOUNT_UPDATE ? 'highlightSideNav' : ''}`} onClick={() => handleClick(SharedVarConstants.NAV_MENU_SELECT_ACCOUNT_UPDATE)}>{SharedVarConstants.ACCOUNT_UPDATE_HEADER}</li>
+                <li className={`sidebar-item ${accountMenuSelect === SharedVarConstants.NAV_MENU_SELECT_TRX_HIST ? 'highlightSideNav' : ''}`} onClick={() => handleClick(SharedVarConstants.NAV_MENU_SELECT_TRX_HIST)}>{SharedVarConstants.TRANSACTION_HISTORY_HEADER}</li>
+                <li className={`sidebar-item ${accountMenuSelect === SharedVarConstants.NAV_MENU_SELECT_LIMIT_MGMT ? 'highlightSideNav' : ''}`} onClick={() => handleClick(SharedVarConstants.NAV_MENU_SELECT_LIMIT_MGMT)}>{SharedVarConstants.LIMIT_MANAGEMENT_HEADER}</li>
+                <li className={`sidebar-item ${accountMenuSelect === SharedVarConstants.NAV_MENU_SELECT_DEPOSITS ? 'highlightSideNav' : ''}`} onClick={() => handleClick(SharedVarConstants.NAV_MENU_SELECT_DEPOSITS)}>{SharedVarConstants.DEPOSIT_HEADER}</li>
+                <li className={`sidebar-item ${accountMenuSelect === SharedVarConstants.NAV_MENU_SELECT_WITHDRAWALS ? 'highlightSideNav' : ''}`} onClick={() => handleClick(SharedVarConstants.NAV_MENU_SELECT_WITHDRAWALS)}>{SharedVarConstants.WITHDRAWALS_HEADER}</li>
+            </ul>
+        </div>
+    );
+}
+  
+export default AccountSideNav;
