d6cf6aa4317e94c26e2a0f3c52fa0c6cd56b323f
Vincent Lai <vincentlai91@hotmail.com>
2024-05-19 16:09:54 +0800

    added validations for create user form

--- oasisbet-react/src/component/account/CreateUser.tsx
+++ oasisbet-react/src/component/account/CreateUser.tsx
@@ -9,80 +9,148 @@ const CreateUser = () => {
   const [email, setEmail] = useState('');
   const [contactNo, setContactNo] = useState('');
   const [errors, setErrors] = useState({
-    usernameError: '',
-    passwordError: '',
-    cfmPasswordError: '',
-    emailError: '',
-    contactNoError: ''
+    username: '',
+    password: '',
+    cfmPassword: '',
+    email: '',
+    contactNo: ''
   });
 
-  const handleValidation = () => {
+  const handleValidation = (inputType) => {
     let validationErrors = {
-      usernameError: '',
-      passwordError: '',
-      cfmPasswordError: '',
-      emailError: '',
-      contactNoError: ''
+      username: '',
+      password: '',
+      cfmPassword: '',
+      email: '',
+      contactNo: ''
     };
 
-    if (!username) {
-      validationErrors.usernameError = 'This field is required';
-    } else if (!/^[a-zA-Z]+$/.test(username)) {
-      validationErrors.usernameError = 'Please enter only alphabet characters';
-    } else if (username.length > 20) {
-      validationErrors.usernameError = 'Maximum length is 20 characters';
-    } else if (username.length < 5) {
-      validationErrors.usernameError = 'Minimum length is 5 characters';
+    if(inputType === 'username'){
+      validationErrors.username = validateUsername(username);
+    }
+    
+    if(inputType === 'password'){
+      validationErrors.password = validatePassword(password);
+    }
+    
+    if(inputType === 'cfmPassword'){
+      validationErrors.cfmPassword = validateCfmPassword(cfmPassword);
     }
 
-    if (!password) {
-      validationErrors.passwordError = 'This field is required';
-    } else if (password.length > 20) {
-      validationErrors.passwordError = 'Maximum length is 20 characters';
-    } else if (password.length < 5) {
-      validationErrors.passwordError = 'Minimum length is 5 characters';
+    if(inputType === 'email'){
+      validationErrors.email = validateEmail(email);
+    }
+    
+    if(inputType === 'contactNo'){
+      validationErrors.contactNo = validateContactNo(contactNo);
     }
 
-    if (!cfmPassword) {
-      validationErrors.cfmPasswordError = 'This field is required';
-    } else if (cfmPassword !== password) {
-      validationErrors.cfmPasswordError = 'Passwords do not match';
-    }
-
-    if (!email) {
-      validationErrors.emailError = 'This field is required';
-    } else if (!/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-      validationErrors.emailError = 'Please enter a valid email address';
-    } else if (email.length > 100) {
-      validationErrors.emailError = 'Maximum length is 100 characters';
-    }
-
-    if (!contactNo) {
-      validationErrors.contactNoError = 'This field is required';
-    } else if (!/^\d+$/.test(contactNo)) {
-      validationErrors.contactNoError = 'Please enter only numeric characters';
-    } else if (contactNo.length > 30) {
-      validationErrors.contactNoError = 'Maximum length is 30 characters';
-    }
-    console.log("form validation errors: ", validationErrors);
     setErrors(validationErrors);
-    return Object.values(validationErrors).every(error => error === '');
   };
 
   const confirmClicked = () => {
-    if (handleValidation()) {
-      console.log('Form is valid');
+      const validationErrors = {
+        username: '',
+        password: '',
+        cfmPassword: '',
+        email: '',
+        contactNo: ''
+      };
+      validationErrors.username = validateUsername(username);
+      validationErrors.password = validatePassword(password);
+      validationErrors.cfmPassword = validateCfmPassword(cfmPassword);
+      validationErrors.email = validateEmail(email);
+      validationErrors.contactNo = validateContactNo(contactNo);
+      setErrors(validationErrors);
+      const checkValidation = Object.values(validationErrors).every(error => error === '');
+    if (checkValidation) {
+      console.log('Form is valid, submitting form to backend now');
       // Handle form submission
     } else {
       console.log('Form is invalid');
     }
   };
 
-  function handleOnChangeUsername(e): void {
-    setUsername(e.target.value.toUpperCase());
-    errors.usernameError = '';
-    console.log("testing on change username: ", errors);
-    setErrors(errors);
+  const handleOnBlurInput = (inputType) => (e) => {
+    handleValidation(inputType);
+  }
+
+  const handleOnChangeInput = (inputType) => (e) => {
+    if(inputType === 'username'){
+      setUsername(e.target.value.toUpperCase());
+    } else if(inputType === 'password'){
+      setPassword(e.target.value);
+    } else if(inputType === 'cfmPassword'){
+      setCfmPassword(e.target.value);
+    } else if(inputType === 'email'){
+      setEmail(e.target.value.toUpperCase());
+    } else if(inputType === 'contactNo'){
+      setContactNo(e.target.value);
+    }
+    setErrors((prevState) => ({
+      ...prevState, 
+      [inputType]: ''
+    }));
+  }
+
+  function validateUsername(username: string): string {
+    if (!username) {
+      return 'This field is required';
+    } else if (!/^[a-zA-Z]+$/.test(username)) {
+      return 'Please enter only alphabet characters';
+    } else if (username.length > 20) {
+      return 'Maximum length is 20 characters';
+    } else if (username.length < 5) {
+      return 'Minimum length is 5 characters';
+    } else {
+      return '';
+    }
+  }
+
+  function validatePassword(password: string): string {
+    if (!password) {
+      return 'This field is required';
+    } else if (password.length > 20) {
+      return 'Maximum length is 20 characters';
+    } else if (password.length < 5) {
+      return 'Minimum length is 5 characters';
+    } else {
+      return '';
+    }
+  }
+  
+  function validateCfmPassword(cfmPassword: string): string {
+    if (!cfmPassword) {
+      return 'This field is required';
+    } else if (cfmPassword !== password) {
+      return 'Passwords do not match';
+    } else {
+      return '';
+    }
+  }
+
+  function validateEmail(email: string): string {
+    if (!email) {
+      return 'This field is required';
+    } else if (!/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      return 'Please enter a valid email address';
+    } else if (email.length > 100) {
+      return 'Maximum length is 100 characters';
+    } else {
+      return '';
+    }
+  }
+
+  function validateContactNo(contactNo: string): string {
+    if (!contactNo) {
+      return 'This field is required';
+    } else if (!/^\d+$/.test(contactNo)) {
+      return 'Please enter only numeric characters';
+    } else if (contactNo.length > 30) {
+      return 'Maximum length is 30 characters';
+    } else {
+      return '';
+    }
   }
 
   return (
@@ -97,7 +165,7 @@ const CreateUser = () => {
           <div className="container">
             <form>
               <div className="form-group form-row required col-md-8 align_cont_center">
-                <label id="USERNAME_LABEL_1" htmlFor="username_0_Label" className={`control-label ${errors.usernameError ? 'highlightLabel' : ''}`}>
+                <label id="USERNAME_LABEL_1" htmlFor="username_0_Label" className={`control-label ${errors.username ? 'highlightLabel' : ''}`}>
                   <span id="USERNAME">User Name</span>
                   <span className="mandatory-label"></span>&nbsp;
                 </label>
@@ -106,18 +174,19 @@ const CreateUser = () => {
                   type="text" 
                   className="form-control allCaps" 
                   value={username}
-                  onChange={handleOnChangeUsername} 
+                  onBlur={handleOnBlurInput('username')}
+                  onChange={handleOnChangeInput('username')} 
                   name="username" 
                 />
-                <label id="username_error_0" className={`error-text ${errors.usernameError ? 'highlightLabel' : ''}`} htmlFor="name_0">
-                  {errors.usernameError}
+                <label id="username_error_0" className={`error-text ${errors.username ? 'highlightLabel' : ''}`} htmlFor="name_0">
+                  {errors.username}
                 </label>
               </div>
 
               <br />
 
               <div className="form-group form-row required col-md-8 align_cont_center">
-                <label id="PASSWORD_LABEL_1" htmlFor="password_0_Label" className={`control-label ${errors.passwordError ? 'highlightLabel' : ''}`}>
+                <label id="PASSWORD_LABEL_1" htmlFor="password_0_Label" className={`control-label ${errors.password ? 'highlightLabel' : ''}`}>
                   <span id="PASSWORD">Password</span>
                   <span className="mandatory-label"></span>&nbsp;
                 </label>
@@ -126,18 +195,19 @@ const CreateUser = () => {
                   type="password" 
                   className="form-control allCaps" 
                   value={password}
-                  onChange={(e) => setPassword(e.target.value)} 
+                  onBlur={handleOnBlurInput('password')}
+                  onChange={handleOnChangeInput('password')} 
                   name="password" 
                 />
-                <label id="password_error_0" className={`error-text ${errors.passwordError ? 'highlightLabel' : ''}`} htmlFor="password_0">
-                  {errors.passwordError}
+                <label id="password_error_0" className={`error-text ${errors.password ? 'highlightLabel' : ''}`} htmlFor="password_0">
+                  {errors.password}
                 </label>
               </div>
 
               <br />
 
               <div className="form-group form-row required col-md-8 align_cont_center">
-                <label id="CFMPASSWORD_LABEL_1" htmlFor="cfmPassword_0_Label" className={`control-label ${errors.cfmPasswordError ? 'highlightLabel' : ''}`}>
+                <label id="CFMPASSWORD_LABEL_1" htmlFor="cfmPassword_0_Label" className={`control-label ${errors.cfmPassword ? 'highlightLabel' : ''}`}>
                   <span id="CFMPASSWORD">Confirm Password</span>
                   <span className="mandatory-label"></span>&nbsp;
                 </label>
@@ -146,18 +216,19 @@ const CreateUser = () => {
                   type="password" 
                   className="form-control allCaps" 
                   value={cfmPassword}
-                  onChange={(e) => setCfmPassword(e.target.value)} 
+                  onBlur={handleOnBlurInput('cfmPassword')}
+                  onChange={handleOnChangeInput('cfmPassword')} 
                   name="cfmPassword" 
                 />
-                <label id="cfmPassword_error_0" className={`error-text ${errors.cfmPasswordError ? 'highlightLabel' : ''}`} htmlFor="cfmPassword_0">
-                  {errors.cfmPasswordError}
+                <label id="cfmPassword_error_0" className={`error-text ${errors.cfmPassword ? 'highlightLabel' : ''}`} htmlFor="cfmPassword_0">
+                  {errors.cfmPassword}
                 </label>
               </div>
 
               <br />
 
               <div className="form-group form-row required col-md-8 align_cont_center">
-                <label id="EMAIL_LABEL_1" htmlFor="email_0_Label" className={`control-label ${errors.emailError ? 'highlightLabel' : ''}`}>
+                <label id="EMAIL_LABEL_1" htmlFor="email_0_Label" className={`control-label ${errors.email ? 'highlightLabel' : ''}`}>
                   <span id="EMAIL">Email</span>
                   <span className="mandatory-label"></span>&nbsp;
                 </label>
@@ -166,18 +237,19 @@ const CreateUser = () => {
                   type="text" 
                   className="form-control allCaps" 
                   value={email}
-                  onChange={(e) => setEmail(e.target.value)} 
+                  onBlur={handleOnBlurInput('email')}
+                  onChange={handleOnChangeInput('email')} 
                   name="email" 
                 />
-                <label id="email_error_0" className={`error-text ${errors.emailError ? 'highlightLabel' : ''}`} htmlFor="email_0">
-                  {errors.emailError}
+                <label id="email_error_0" className={`error-text ${errors.email ? 'highlightLabel' : ''}`} htmlFor="email_0">
+                  {errors.email}
                 </label>
               </div>
 
               <br />
 
               <div className="form-group form-row required col-md-8 align_cont_center">
-                <label id="CONTACTNO_LABEL_1" htmlFor="contactNo_0_Label" className={`control-label ${errors.contactNoError ? 'highlightLabel' : ''}`}>
+                <label id="CONTACTNO_LABEL_1" htmlFor="contactNo_0_Label" className={`control-label ${errors.contactNo ? 'highlightLabel' : ''}`}>
                   <span id="CONTACTNO">Contact No</span>
                   <span className="mandatory-label"></span>&nbsp;
                 </label>
@@ -186,11 +258,12 @@ const CreateUser = () => {
                   type="text" 
                   className="form-control allCaps" 
                   value={contactNo}
-                  onChange={(e) => setContactNo(e.target.value)} 
+                  onBlur={handleOnBlurInput('contactNo')}
+                  onChange={handleOnChangeInput('contactNo')} 
                   name="contactNo" 
                 />
-                <label id="contactNo_error_0" className={`error-text ${errors.contactNoError ? 'highlightLabel' : ''}`} htmlFor="contactNo_0">
-                  {errors.contactNoError}
+                <label id="contactNo_error_0" className={`error-text ${errors.contactNo ? 'highlightLabel' : ''}`} htmlFor="contactNo_0">
+                  {errors.contactNo}
                 </label>
               </div>
 
@@ -209,3 +282,5 @@ const CreateUser = () => {
 };
 
 export default CreateUser;
+
+
