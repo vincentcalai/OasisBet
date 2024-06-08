f389cc5cf45c5cf91a5ee2568c64b73159b32ba0
Vincent Lai <vincentlai91@hotmail.com>
2024-06-08 15:40:39 +0800

    added AccountOverview UI

--- oasisbet-react/src/component/account/AccountOverview.tsx
+++ oasisbet-react/src/component/account/AccountOverview.tsx
@@ -11,7 +11,30 @@ export default function AccountOverview(){
                     <h2>Account Overview</h2>
                 </Card.Header>
                 <Card.Body className="card-body">
-                    
+                <label className="control-label">
+                    Balance
+                </label>
+                <div className="dashboard-sidemenu-value">
+                    $100.00
+                </div>
+                <label className="control-label">
+                    Deposit
+                </label>
+                <div className="dashboard-sidemenu-value">
+                    $50.00
+                </div>
+                <div className="text_year_to_date">
+                    (Year To Date)
+                </div>
+                <label className="control-label">
+                    Withdrawal
+                </label>
+                <div className="dashboard-sidemenu-value">
+                    $28.00
+                </div>
+                <div className="text_year_to_date">
+                    (Year To Date)
+                </div>
                 </Card.Body> 
             </Card>
         </div>
