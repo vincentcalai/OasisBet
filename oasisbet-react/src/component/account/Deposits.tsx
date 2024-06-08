227f648a09cc71409ac9583fcb93ec92c735b972
Vincent Lai <vincentlai91@hotmail.com>
2024-06-08 16:38:15 +0800

    added Deposit and Withdrawal UI and css

--- oasisbet-react/src/component/account/Deposits.tsx
+++ oasisbet-react/src/component/account/Deposits.tsx
@@ -11,7 +11,55 @@ export default function Deposits(){
                     <h2>Deposits</h2>
                 </Card.Header>
                 <Card.Body className="card-body">
-                    
+                    <label className="control-label col-xs-6 col-sm-3 col-md-3 deposit-section-label-width">Balance:</label>
+                        <span className="col-xs-6 col-sm-3 col-md-2 deposit-section-selection-width">$100.00</span>
+                        <br />
+                        <br />
+                    <label className="control-label col-xs-6 col-sm-3 col-md-3 deposit-section-label-width">Deposit Remaining Limit:</label>
+                    <span className="col-xs-6 col-sm-3 col-md-2 deposit-section-selection-width">
+                        $1000.00
+                    </span>
+                    <br />
+                    <br />
+                    <span className="control-label check-trx-hist-note">
+                        Note: Check <a href="#transaction-history" onClick={() => { /* Add logic to navigate to transaction history */ }}>transaction history</a> for more details.
+                    </span>
+                    <hr />
+                    <form>
+                        <div className="form-group row">
+                            <label
+                                id="DEPOSIT_LABEL_1"
+                                htmlFor="deposit_0_Label"
+                                className="control-label col-sm-4 deposit-section-label-width deposit-acc-label-text"
+                            >
+                                <span id="DEPOSIT">Deposit:</span>
+                            </label>
+                            <div className="col-sm-4">
+                                <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text">$</span>
+                                </div>
+                                <input
+                                    id="depositAmt_0"
+                                    type="text"
+                                    className="form-control allCaps"
+                                    name="depositAmt"
+                                    inputMode="numeric"
+                                />
+                                </div>
+                            </div>
+                            </div>
+                        </form>
+                    <hr />
+                    <div className="dialog-actions">
+                        <button className="btn btn-danger btn-cancel" type="button">
+                        Cancel
+                        </button>
+                        <button className="btn btn-success btn-confirm" type="button">
+                        Confirm
+                        </button>
+                    </div>
+                    <br />
                 </Card.Body> 
             </Card>
         </div>
--- oasisbet-react/src/component/account/Withdrawals.tsx
+++ oasisbet-react/src/component/account/Withdrawals.tsx
@@ -11,7 +11,51 @@ export default function Withdrawals(){
                     <h2>Withdrawals</h2>
                 </Card.Header>
                 <Card.Body className="card-body">
-                    
+                <label className="control-label col-xs-6 col-sm-3 col-md-3 withdraw-section-label-width">Balance:</label>
+                    <span className="col-xs-6 col-sm-3 col-md-2 withdraw-section-selection-width">$100.00</span>
+                    <br />
+                    <br />
+                    <span className="control-label check-trx-hist-note">
+                        Note: Check <a href="#trxHist" onClick={() => {/* navToTrxHistMenu() */}}>transaction history</a> for more details.
+                    </span>
+                    <hr />
+                    <form>
+                        <div className="form-group row">
+                        <label id="WITHDRAWAL_LABEL_1" htmlFor="withdrawal_0_Label" className="control-label col-sm-4 withdraw-section-label-width withdraw-acc-label-text">
+                            <span id="WITHDRAWAL">Withdraw from OasisBet Account:</span>
+                        </label>
+                        <div className="col-sm-4">
+                            <div className="input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text">$</span>
+                            </div>
+                            <input id="withdrawalAmt_0" type="text" className="form-control allCaps" name="withdrawalAmt" />
+                            </div>
+                        </div>
+                        </div>
+                        <br />
+                        <div className="form-group row">
+                        <label id="PASSWORD_LABEL_1" htmlFor="password_0_Label" className="control-label col-sm-4 withdraw-section-label-width withdraw-acc-label-text">
+                            <span id="PASSWORD">Enter OasisBet Account password:</span>
+                        </label>
+                        <div className="col-sm-4">
+                            <div className="input-group">
+                            <div className="input-group-prepend"></div>
+                            <input type="password" className="form-control withdraw-section-selection-width no-spinner" id="password_0" name="password" required />
+                            </div>
+                        </div>
+                        </div>
+                    </form>
+                    <hr />
+                    <div className="dialog-actions">
+                        <button className="btn btn-danger btn-cancel" type="button" onClick={() => {/* onCancelWithdrawal() */}}>
+                        Cancel
+                        </button>
+                        <button className="btn btn-success btn-confirm" type="button" onClick={() => {/* confirmClicked() */}}>
+                        Confirm
+                        </button>
+                    </div>
+                    <br />
                 </Card.Body> 
             </Card>
         </div>
