71d59ba623d632878c23bccc6a79a661d5e1814a
Vincent Lai <vincentlai91@hotmail.com>
2024-07-29 21:46:51 +0800

    fix bug on 1X2 bet type not displayed on UI

--- oasisbet-react/src/constants/SharedVarMethods.ts
+++ oasisbet-react/src/constants/SharedVarMethods.ts
@@ -1,9 +1,10 @@
+import SharedVarConstants from "./SharedVarConstants.ts";
 
 const SharedVarMethods = {
     mapBetTypeCd(betType){
         switch(betType) {
-            case this.BET_TYPE_CD_H2H: {
-                return this.BET_TYPE_H2H_NAME;
+            case SharedVarConstants.BET_TYPE_CD_H2H: {
+                return SharedVarConstants.BET_TYPE_H2H_NAME;
             }
             default: {
             return '';
