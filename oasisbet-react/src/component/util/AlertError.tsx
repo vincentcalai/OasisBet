783bbcbb7aada963285022e87460ab26f77c9686
Vincent Lai <vincentlai91@hotmail.com>
2024-08-08 23:48:45 +0800

    added icon to system down message alert error banner

--- oasisbet-react/src/component/util/AlertError.tsx
+++ oasisbet-react/src/component/util/AlertError.tsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
-import { Alert, Button } from "react-bootstrap";
+import { Alert } from "react-bootstrap";
 import './AlertError.css';
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function AlertError() {
     const [show, setShow] = useState(true);
   
-    if (show) {
       return (
-        <Alert className="alert-component" variant="danger" onClose={() => setShow(false)} dismissible>
-          <span>
-            <b>This system is currently not available. Please try again at a later time.</b>
-            <br />
-            Error:
-          </span>
-        </Alert>
+        show && 
+            <Alert className="alert alert-component view-system-messages" variant="danger" onClose={() => setShow(false)} dismissible>
+                <span>
+                    <FontAwesomeIcon icon={faExclamationTriangle} style={{ marginRight: '8px' }} /> 
+                    <b>This system is currently not available. Please try again at a later time.</b>
+                    <br />
+                    Error:
+                </span>
+            </Alert>
       );
-    }
 }
   
   export default AlertError;
