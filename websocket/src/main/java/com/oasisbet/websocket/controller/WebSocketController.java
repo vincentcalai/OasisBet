c9bf1648d8e2f3c3bf6c63d5843c418a08b4a0c7
Vincent Lai <vincentlai91@hotmail.com>
2024-12-15 13:30:53 +0800

    revert betEventJson to BetEvent

--- websocket/src/main/java/com/oasisbet/websocket/controller/WebSocketController.java
+++ websocket/src/main/java/com/oasisbet/websocket/controller/WebSocketController.java
@@ -1,6 +1,5 @@
 package com.oasisbet.websocket.controller;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oasisbet.websocket.model.BetEvent;
 import lombok.extern.slf4j.Slf4j;
@@ -8,20 +7,18 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
-
 @Controller
 @Slf4j
 public class WebSocketController {
 
     @MessageMapping("/betting")
     @SendToUser("/topic/odds")
-    public List<BetEvent> oddsTopicWebSocket(String betEventJson) {
+    public BetEvent oddsTopicWebSocket(String betEventJson) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules(); // Handles Java 8 date and time serialization
-        List<BetEvent> betEvent = null;
+        BetEvent betEvent = null;
         try {
-            betEvent = objectMapper.readValue(betEventJson, new TypeReference<List<BetEvent>>() {});
+            betEvent = objectMapper.readValue(betEventJson, BetEvent.class);
         } catch (Exception e) {
             log.error("Error in reading Bet Event Json", e);
         }
